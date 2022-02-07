70898a6b39bb4e3cf462a071f2f0eb87a62a68a3
PatrickMSM <calin.chertes@gmail.com>
2022-02-07 12:36:48 +0200

    Add dimension Void

--- src/main/java/tk/patsite/voidmod/VoidMod.java
+++ src/main/java/tk/patsite/voidmod/VoidMod.java
@@ -1,14 +1,42 @@
-package net.fabricmc.example;
+package tk.patsite.voidmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.DimensionRenderingRegistry;
+import net.minecraft.client.render.DimensionEffects;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
 
-public class ExampleMod implements ModInitializer {
+public class VoidMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+
+	public static final String MODID = "voidmod";
+
+	public static final Logger LOGGER = LogManager.getLogger("voidmod");
+
+	public static final Identifier VOID_WORLD_IDENTIFIER = new Identifier("voidmod", "void");
+
+	private static final class VoidWorldDimensionEffects extends DimensionEffects {
+		public VoidWorldDimensionEffects() {
+			super(Float.NaN, false, DimensionEffects.SkyType.NONE, false, false);
+		}
+		@Override public Vec3d adjustFogColor(Vec3d color, float sunHeight) {
+			return color.multiply(0.15f);
+		}
+		@Override public boolean useThickFog(int camX, int camY) {
+			return false;
+		}
+		@Override public float[] getFogColorOverride(float skyAngle, float tickDelta) {
+			return null;
+		}
+	}
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +44,7 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+
+		DimensionRenderingRegistry.registerDimensionEffects(VOID_WORLD_IDENTIFIER, new VoidWorldDimensionEffects());
 	}
 }
